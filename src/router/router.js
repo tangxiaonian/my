479ef44a700d3c820d8ad8098480f35bdad377d3
tang <2684270465@qq.com>
2020-06-25 18:44:52 +0800

    init

--- src/router/router.js
+++ src/router/router.js
@@ -10,7 +10,7 @@ const router = new VueRouter({
         {
             path:"/",
             redirect: () => {
-                return "/index";
+                return "/login";
             }
         },
         {
